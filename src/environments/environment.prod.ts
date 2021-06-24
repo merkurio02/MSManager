28cd366fd36a81e73ab7fcc9ad68a4b699071492
merkurio02 <alejandro.esp.silv@gmail.com>
2021-06-23 23:51:02 -0400

    Configuraciones para deploy en gh-pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyArgD7WQUA9mGpb68rJY4XTVlGx77UrjDs",
+    authDomain: "makensell.firebaseapp.com",
+    databaseURL: "https://makensell-default-rtdb.firebaseio.com",
+    projectId: "makensell",
+    storageBucket: "makensell.appspot.com",
+    messagingSenderId: "197424589343",
+    appId: "1:197424589343:web:f55939ec3c4016ccaa032a",
+    measurementId: "G-T3LGSFL3L8"
+  }
 };
